8df4a68b53b269ee87249db5d93962843a0df391
Rafael Suarez <rafaelv_s@hotmail.com>
2022-05-30 09:09:52 -0300

    criação do projeto

---
+++ ClinicaMultiAfetos/ClinicaMultiAfetos/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using ClinicaMultiAfetos.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ClinicaMultiAfetos.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Agenda()
+        {
+            return View();
+        }
+
+        public IActionResult Pacientes()
+        {
+            return View();
+        }
+
+        public IActionResult Documentos()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
