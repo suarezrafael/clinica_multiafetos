b689601da0b92fdea327eb6237104fa89f6da628
Rafael Suarez <rafaelv_s@hotmail.com>
2022-06-01 16:07:32 -0300

    configurando deploy do banco.

--- ClinicaMultiAfetos/ClinicaMultiAfetos/Context/AppDbContext.cs
+++ ClinicaMultiAfetos/ClinicaMultiAfetos/Context/AppDbContext.cs
@@ -5,7 +5,7 @@ namespace ClinicaMultiAfetos.Context
 {
     public class AppDbContext : DbContext
     {
-        public AppDbContext(DbContextOptions<DbContext> options) : base(options)
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
 
         }
